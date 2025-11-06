7b52d5d4a5f6ed5f918f5a39d0492d89392b0fb4
Arjunn786 <anandarjun073@gmail.com>
2025-11-06 17:38:25 +0530

    ci cd pileline

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get('/api/health', (req, res) => {
 app.get('/api/users', (req, res) => {
   const users = [
     { id: 1, name: 'John Doe', email: 'john@example.com' },
-    { id: 2, name: 'Jane Smith', email: 'jane@example.com' },
+    { id: 2, name: 'shivam Smith', email: 'jane@example.com' },
     { id: 3, name: 'Bob Johnson', email: 'bob@example.com' }
   ];
   res.json({ users, count: users.length });
